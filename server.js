004e33d3636e1ef094a5d77b0802f0823ca6aec0
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-05-29 07:41:51 -0300

    Adding mongoose dependency

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.write("Hello, World!");
-        res.end();
-    }
+const _ = require("underscore");
 
-    if (req.url === "/api/courses") {
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
-});
-
-server.listen(3000);
-
-console.log("Listening on port 3000...");
+const result = _.contains([1, 2, 3], 2);
+console.log("Result:", result);
